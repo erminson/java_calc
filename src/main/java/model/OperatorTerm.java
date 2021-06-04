76689fda8b2103159d65a524cae60003261055e8
Lev Erminson <erminson@gmail.com>
2021-06-04 13:05:00 +0300

    OperatorTerm: override  and  methods

--- src/main/java/model/OperatorTerm.java
+++ src/main/java/model/OperatorTerm.java
@@ -13,6 +13,21 @@ public class OperatorTerm extends Term {
     }
 
     @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        OperatorTerm that = (OperatorTerm) o;
+
+        return operatorType == that.operatorType;
+    }
+
+    @Override
+    public int hashCode() {
+        return operatorType != null ? operatorType.hashCode() : 0;
+    }
+
+    @Override
     public String toString() {
         return operatorType.toString();
     }
